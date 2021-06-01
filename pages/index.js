fc26f2528cefa9ced7610dfb6785fa1196018fd9
nhtwang <85042551+nhtwang@users.noreply.github.com>
2021-06-01 15:23:01 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,18 +20,6 @@ export default function Home() {
             <p>Read the terms & conditions of becoming a registrant of .网 domains here!</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
