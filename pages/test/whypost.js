a96121fe041f3ec76dd671fc6f74ea7dc5fc6989
nhtwang <85042551+nhtwang@users.noreply.github.com>
2021-06-02 16:28:14 +1000

    Update whypost.js

--- pages/test/whypost.js
+++ pages/test/whypost.js
@@ -1,7 +1,4 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
+export default function WhyPost() {
   return (
     <div className={styles.container}>
       <Head>
