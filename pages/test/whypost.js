0ecc5ec9bf68c24f4e660890ef9c74049d2a2741
nhtwang <85042551+nhtwang@users.noreply.github.com>
2021-06-02 16:45:21 +1000

    Update whypost.js

--- pages/test/whypost.js
+++ pages/test/whypost.js
@@ -1,14 +1,3 @@
 export default function WhyPost() {
-  return 
-    <div className={styles.container}>
-      <Head>
-        <title>Why .网?</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
- 
-        <h1>
-          Why .网
-        </h1>
-
-  
+  return <h1> Why .网 </h1>
 }
